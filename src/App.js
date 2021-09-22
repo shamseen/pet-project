721e61686a9bee94387d89f791d9253692b8fcda
Benji <bwolfenberger@gmail.com>
2021-09-22 11:18:50 -0700

    set up function to pull data from mock file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React,{useState,useEffect} from 'react';
 import './App.css';
-import About from './views/About'
-import Results from './views/Results'
 
 function App() {
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('mockData.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <div className="App">
-      <About />
-      <Results />
+     {
+       data && data.length>0 && data.map((item)=><p>{item.about}</p>)
+     }
     </div>
   );
 }
