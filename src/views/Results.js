6d8ab58a2ff9bb26f5c61b73a2a37006fc1d5400
Benji <bwolfenberger@gmail.com>
2021-09-22 11:56:06 -0700

    data displayed on results page

--- src/views/Results.js
+++ src/views/Results.js
@@ -1,11 +1,24 @@
 
 
-// export default function Results() {
-//   return (
-//     <div className="App">
-//      {
-//        data && data.length>0 && data.map((item)=><p>{item.about}</p>)
-//      }
-//     </div>
-//  );
-// }
+export default function Results(props) {
+  console.log(props.mockData)
+
+
+  const renderedData = props.mockData.map(data => {
+    return(
+        <ul>
+            <span id="animal">Animal: {data.Animal} <br></br></span>
+            <span id="species">Breed: {data.Breed} <br></br></span>
+            <span id="age">Age: {data.Age}</span>
+        </ul>
+    )
+})
+
+  return (
+    <div>
+
+      {renderedData}
+
+    </div>
+ );
+}
