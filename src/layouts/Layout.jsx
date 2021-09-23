3aef8f6736ff5d0dc6cb51320406ac01dacb24e7
Ashley Sands <ashley.sands01@gmail.com>
2021-09-22 19:44:56 -0500

    working on landing page, inputs are just for layout purposes for now

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,10 +5,17 @@ import "./Layout.css"
 export default function Layout(props) {
   const { user, handleLogout } = props;
   return (
-    <header>
+    <>
       <div className='nav-header'>
-        <div className='website-name'>
-          <NavLink exact to='/'><h1>PURRPOSEFUL PAWS</h1></NavLink>
+        <div className='title-search'>
+          <div className='website-name'>
+            <NavLink exact to='/'><h1>PURRPOSEFUL PAWS</h1></NavLink>
+          </div>
+          <div>
+            <input
+              className='nav-search'
+              placeholder='WHAT PET ARE YOU LOOKING FOR?' />
+          </div>
         </div>
         <div className='nav-links'>
           {user ? (
@@ -17,6 +24,7 @@ export default function Layout(props) {
               <NavLink className='nav-button' to='/about'>ABOUT</NavLink>
               <NavLink className='nav-profile' to={`/profile/${user.name}`}>{(user.name).toUpperCase()}</NavLink>
               <button className='nav-button' onClick={handleLogout}>LOGOUT</button>
+              <NavLink className='nav-button work-with-button' to='/find-pet'>WORK WITH US</NavLink>
 
             </>
           ) : (
@@ -25,11 +33,12 @@ export default function Layout(props) {
               <NavLink className='nav-button' to='/about'>ABOUT</NavLink>
               <NavLink className='nav-button' to='/login'>LOGIN</NavLink>
               <NavLink className='nav-button' to='/sign-up'>SIGN UP</NavLink>
+              <NavLink className='nav-button work-with-button' to='/find-pet'>WORK WITH US</NavLink>
             </>
           )}
         </div>
       </div>
       {props.children}
-    </header>
+    </>
   )
 }
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import './Landing.css'
+import dogLarge from '../assets/dogLarge.png'
+import dogSmall from '../assets/dogSmall.png'
 
 export default function Landing() {
   return (
-    <div>
-      LANDING
+    <div className='landing-test'>
+      <div className='landing-container'>
+        <div className='landing-text'>
+          <h1 className='landing-main-text'>Animals that need you most</h1>
+          <h2 className='landing-sub-text'>We’re changing the way the world sees and treats unwanted animals.</h2>
+          <h2 className='landing-sub-text'>We specialize in giving animals that have <span>special needs</span> or that are in their <span>senior
+            years</span> a chance at a loving home. Be our dogs and cats fur-ever home today.</h2>
+          <button className='landing-button'>FIND YOUR PERFECT MATCH</button>
+        </div>
+        <div className='landing-image'>
+          <img className='landing-image-large' src={dogLarge} alt='dogs sitting on and in front of a couch' />
+          <img className='landing-image-small' src={dogSmall} alt='person holding dog' />
+        </div>
+      </div>
+      <div className='landing-bottom'>
+        I'm looking for
+        <br />
+        Dogs
+        Cats
+        <br />
+        <div className='inputs'>
+          location
+          <input />
+          age
+          <input />
+          breed
+          <input />
+          <button>search</button>
+        </div>
+      </div>
     </div>
   )
 }
