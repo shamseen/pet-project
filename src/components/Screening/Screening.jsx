fe667eda73c38dcc341caed9460245b046567745
Ashley Sands <ashley.sands01@gmail.com>
2021-09-23 11:23:37 -0500

    started screening modal

---
+++ src/components/Screening/Screening.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import * as React from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import '../../components/SignUp/SignUp.css'
+
+
+export default function Sreening(props) {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <button className='landing-button' onClick={handleClickOpen}>
+        FIND YOUR PERFECT MATCH
+      </button>
+      <Dialog className='dialog' open={open} onClose={handleClose}>
+        <div className='dialog-left-box'>
+          <h1 className='modal-left-title'>How it works</h1>
+          <ol>
+            <li><div><>Fill out a 60 sec screener</>
+              <p>Tell us about yourself and the pet you’re looking for</p>
+            </div>
+            </li>
+            <li><div><>Get matched with your future pet</>
+              <p>We’ll match you with your ideal pet according to your screener </p>
+            </div>
+            </li>
+            <li><div><>Take your purrposeful pal home</>
+              <p>We’ll guide you to the shelter or organization that will help you take your new pet home.</p>
+            </div>
+            </li>
+          </ol>
+        </div>
+        <div className='dialog-right-box'>
+          <h1 className='modal-right-title'>Find your perfect match</h1>
+          <DialogContent>
+            <div className='dialog-top'>
+              <p>To help you find your perfect match, tell us about yourself and the pet you’re looking for. Not sure for now? skip and go back later.
+              </p>
+            </div>
+            <input
+              className='dialog-input'
+              margin="dense"
+              id="location"
+              label="Location"
+              type="text"
+              fullWidth
+              // value={form.name}
+              placeholder="Location"
+            // onChange={handleInput}
+            />
+            <input
+              className='dialog-input'
+              margin="dense"
+              id="desired-pet"
+              label="Desired Pet"
+              type="text"
+              fullWidth
+              // value={form.email}
+              placeholder="Desired Pet"
+              autoComplete="off"
+            // onChange={handleInput}
+            />
+            <input
+              className='dialog-input'
+              margin="dense"
+              id="breed"
+              label="Breed"
+              type="text"
+              fullWidth
+              // value={form.password}
+              placeholder="Breed"
+              autoComplete="off"
+            // onChange={handleInput}
+            />
+          </DialogContent>
+          <div className='sign-up-buttons'>
+            <button
+              className='sign-up'
+              onClick={handleClose}>CONTINUE</button>
+            <button className='guest' onClick={handleClose}>Skip</button>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -5,6 +5,7 @@ import dogSmall from '../assets/dogSmall.png'
 import LandingForm from '../components/Landing/LandingForm'
 import Section2 from '../components/Landing/Section2'
 import Section3 from '../components/Landing/Section3'
+import Screening from '../components/Screening/Screening'
 
 export default function Landing() {
 
@@ -17,7 +18,7 @@ export default function Landing() {
             <h2 className='landing-sub-text'>We’re changing the way the world sees and treats unwanted animals.</h2>
             <h2 className='landing-sub-text'>We specialize in giving animals that have <span>special needs</span> or that are in their <span>senior
               years</span> a chance at a loving home. Be our dogs and cats fur-ever home today.</h2>
-            <button className='landing-button'>FIND YOUR PERFECT MATCH</button>
+            <Screening />
           </div>
           <div className='landing-image-container'>
             <img className='landing-image-large' src={dogLarge} alt='dogs sitting on and in front of a couch' />
